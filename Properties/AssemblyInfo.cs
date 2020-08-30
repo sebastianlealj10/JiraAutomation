12ea82e3aaf15772a23e907510e40365b3f593d1
Sebastian Leal <sebastianleaj10@gmail.com>
2020-08-30 14:24:50 -0500

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JiraAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JiraAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b584b8b-37db-46bd-8bf1-9cdfd34bd776")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace JiraAutomationTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var driver = new ChromeDriver
+            {
+                Url = "http://localhost:8080/"
+            };
+            driver.Manage().Window.Maximize();
+            var username = driver.FindElementById("login-form-username");
+            username.SendKeys("sebas.adm1n10");
+            var password = driver.FindElementById("login-form-password");
+            password.SendKeys("sebas12");
+            var loguinButton = driver.FindElementById("login");
+            loguinButton.Click();
+            Thread.Sleep(1000);
+            //driver.Manage().Window.FullScreen();
+            Thread.Sleep(5000);
+        }
+    }
+}
