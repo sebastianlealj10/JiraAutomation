a175e7c7e870dff53cce9e2e45b9e15f1cd889ad
Sebastian Leal <sebastianleaj10@gmail.com>
2020-08-30 21:00:13 -0500

    POM

---
+++ ClassLibrary1/LoginPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class LoginPage
+    {
+        IWebDriver driver;
+        private IWebElement Username => driver.FindElement(By.Id("login-form-username"));
+        private IWebElement Password => driver.FindElement(By.Id("login-form-password"));
+
+        private IWebElement LoguinButton => driver.FindElement(By.Id("login"));
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void fillUsername(string username)
+        {
+            Username.SendKeys(username);
+        }
+
+        public void fillPassword(string password)
+        {
+            Password.SendKeys(password);
+        }
+
+        public void ClickLogin()
+        {
+            LoguinButton.Click();
+        }
+
+
+    }
+}
---
+++ JiraTests.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibrary1;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace JiraAutomationTests
+{
+    [TestFixture]
+    public class JiraTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var driver = new ChromeDriver
+            {
+                Url = "http://localhost:8080/"
+            };
+            driver.Manage().Window.Maximize();
+
+            var loginPage = new LoginPage(driver);
+            loginPage.fillUsername("sebas.adm1n10");
+            loginPage.fillPassword("sebas12");
+            loginPage.ClickLogin();
+
+
+            Thread.Sleep(5000);
+        }
+    }
+}
