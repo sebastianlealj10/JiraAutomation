a48965dda03c6111043338074597f4794e861d6c
Sebastian Leal <juan.leal@endava.com>
2020-09-05 12:24:50 -0500

    API abstraction

--- APITests.cs
+++ APITests.cs
@@ -1,8 +1,9 @@
 ﻿using NUnit.Framework;
 using RestSharp;
 using RestSharp.Authenticators;
-using System;
 using System.Configuration;
+using ClassLibrary1;
+using System;
 
 namespace JiraAutomation
 {
@@ -10,22 +11,25 @@ namespace JiraAutomation
     public class APITests
     {
         private readonly string BaseUrl = ConfigurationManager.AppSettings["BaseUrl"];
-        private readonly string IssueEndpoint = ConfigurationManager.AppSettings["IssueEndpoint"];
         private readonly string ConfigUsername = ConfigurationManager.AppSettings["Username"];
         private readonly string ConfigPassword = ConfigurationManager.AppSettings["Password"];
+
+        RestClient restClient;
+
+        [SetUp]
+        public void setUp()
+        {
+            restClient = new RestClient(BaseUrl)
+            {
+                Authenticator = new HttpBasicAuthenticator(ConfigUsername, ConfigPassword),
+            };
+        }
 
         [Test]
         public void ANewEpicIsCreatedUsingTheAPI()
         {
-            var restClient = new RestClient(BaseUrl)
-            {
-                Authenticator = new HttpBasicAuthenticator(ConfigUsername, ConfigPassword),
-            };
-            var request = new RestRequest(IssueEndpoint);
-            string body = "{\"fields\": {\"project\":{\"key\": \"DEMO\"},\"summary\": \"My Epic created from the API\",\"description\": \"Creating of an Epic using project keys and issue type names using the REST API\",\"issuetype\": {\"name\": \"Epic\"},\r\n\"customfield_10104\": \"MyEpic\"}}";
-            request.AddParameter("application/json; charset=utf-8", body, ParameterType.RequestBody);
-            var response = restClient.Post(request);
-            Console.WriteLine(response.Content);
+            var issue = new Issue(restClient);
+            Console.WriteLine(issue.CreateIssue("Epic"));
         }
     }
 }
---
+++ ClassLibrary1/APIWorkflow/Issue.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Issue
+    {
+        private readonly string IssueEndpoint = ConfigurationManager.AppSettings["IssueEndpoint"];
+        RestClient restClient;
+        public Issue(RestClient restClient)
+        {
+            this.restClient = restClient;
+        }
+
+        public string CreateIssue (string issueType)
+        {
+            var request = new RestRequest(IssueEndpoint);
+            string body = "{\"fields\": {\"project\":{\"key\": \"DEMO\"},\"summary\": \"My Epic created from the API\",\"description\": \"Creating of an Epic using project keys and issue type names using the REST API\",\"issuetype\": {\"name\": \""+issueType+ "\"},\r\n\"customfield_10104\": \"MyEpic\"}}";
+            request.AddParameter("application/json; charset=utf-8", body, ParameterType.RequestBody);
+            return restClient.Post(request).Content;
+        }
+
+    }
+}
